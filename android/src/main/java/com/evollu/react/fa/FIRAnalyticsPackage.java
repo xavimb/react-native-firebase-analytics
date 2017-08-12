05488ad04d87e1ff79554df766d7120f6df2a672
Xavi Magrinyà <xavimb@users.noreply.github.com>
2017-08-12 18:38:59 +0300

    Update FIRAnalyticsPackage.java

--- android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
+++ android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
@@ -25,11 +25,6 @@ public class FIRAnalyticsPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
